1a7a8b10948169f6239f7ae9517d2417d229890d
Ilham Akbar <milham.nj25@gmail.com>
2024-09-14 15:36:15 +0700

    add footer

--- components/HomeComponent/about-section.tsx
+++ components/HomeComponent/about-section.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import bgship from "@/app/images/ship-bg.jpg";
-import Image from "next/image";
+// import bgship from "@/app/images/ship-bg.jpg";
+// import Image from "next/image";
 
 const AboutSection = () => {
   return (
