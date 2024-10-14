b071f8d31663f4e3de598ecfcfad4257ea92ad80
Muhammad Ilham Akbar <muhammad.ilham@os-selnajaya.com>
2024-10-14 16:08:23 +0700

    fix mobile and browser view

--- lib/ChatWhatsapp.tsx
+++ lib/ChatWhatsapp.tsx
@@ -1,38 +1,54 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
-// import {mobileCheck} from './mobile-check'
-import { isMobile } from "react-device-detect";
+import React, { useState, useEffect } from "react";
+import { mobileCheck } from "./mobile-check";
+// import { isMobile, BrowserView, MobileView } from "react-device-detect";
 
 interface ChatWhatsappProps {
   children: React.ReactNode;
   phone?: string;
   message?: string;
 }
+
 const ChatWhatsapp = ({
   children,
   phone,
   message,
   ...restProps
 }: ChatWhatsappProps) => {
-  const baseUrl = isMobile
+  const isMobile = mobileCheck();
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    setIsMobileDevice(isMobile);
+  }, []);
+
+  const baseUrl = isMobileDevice
     ? "https://api.whatsapp.com/send"
     : "https://web.whatsapp.com/send";
 
   const queryParams = phone
     ? `?phone=${phone}${message ? `&text=${encodeURIComponent(message)}` : ""}`
     : "";
-
   return (
-    <Link
-      href={baseUrl + queryParams}
-      target="_blank"
-      rel="noreferrer noopener"
-      {...restProps}
-    >
-      {children}
-    </Link>
+    <>
+      <Link
+        href={baseUrl + queryParams}
+        target="_blank"
+        rel="noreferrer noopener"
+        {...restProps}
+      >
+        {children}
+      </Link>
+
+      {/* <BrowserView>
+        <h1 className="text-white">This is rendered only in browser</h1>
+      </BrowserView>
+      <MobileView>
+        <h1 className="">This is rendered only on mobile</h1>
+      </MobileView> */}
+    </>
   );
 };
 
