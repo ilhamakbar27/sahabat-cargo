277c5b5f8b8d115ee9bd5dc3611376f7e6eb31ce
Muhammad Ilham Akbar <muhammad.ilham@os-selnajaya.com>
2024-10-14 17:44:19 +0700

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,21 @@ export default async function sitemap() {
     {
       url: baseUrl,
       lastModified: new Date(),
+      name: "Home",
+    },
+    {
+      url: `${baseUrl}/#services`,
+      lastModified: new Date(),
+      name: "Services",
+      description:
+        "Pelajari lebih lanjut tentang Putra Dunia Cargo dan komitmen kami terhadap layanan berkualitas.",
+    },
+    {
+      url: `${baseUrl}/#contact`,
+      lastModified: new Date(),
+      name: "Contact",
+      description:
+        "Hubungi Putra Dunia Cargo untuk pertanyaan dan dukungan pelanggan.",
     },
   ];
 }
